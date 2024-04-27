37a501b9b6a281f3d9df9872b6247e997ba815c9
Nikita Krenov <nikitabaram@gmail.com>
2024-04-27 16:55:47 +0300

    the 3 exm so

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 
 int main() {
+	int number;
 
+	cout << "Enter a number: ";
+	cin >> number;
+
+	if (number > 0) {
+		cout << "The entered number is positive" << endl;
+	}
+	else if (number < 0) {
+		cout << "The entered number is negative" << endl;
+	}
+	else {
+		cout << "The entered number is zero" << endl;
+	}
 
 	system("pause");
-	return 0
+	return 0;
 }
