795cca116e93795434d55b560f3a4a0f152f2abb
Nikita Krenov <nikitabaram@gmail.com>
2024-04-27 22:51:48 +0200

    yhh, is the last ex today. In this file we can see projram which math liters lose in fly airplane in the manu varius weight

--- main.cpp
+++ main.cpp
@@ -3,8 +3,36 @@
 using namespace std;
 
 int main() {
+	int localAB, localBC, weightOfTheAircraft, fuelConsumption;
 
+	cout << "Enter the distance between points A and B: ";
+	cin >> localAB;
+	cout << "Enter the distance between points B and C: ";
+	cin >> localBC;
+	cout << "Enter the weight of the aircraft: ";
+	cin >> weightOfTheAircraft;
+
+	if (weightOfTheAircraft <= 500) {
+		fuelConsumption = 1;
+	}
+	else if (weightOfTheAircraft <= 1000) {
+		fuelConsumption = 4;
+	}
+	else if (weightOfTheAircraft <= 1500) {
+		fuelConsumption = 7;
+	}
+	else if (weightOfTheAircraft <= 2000) {
+		fuelConsumption = 9;
+	}
+	else {
+		cout << "The plane cannot take off!" << endl;
+		return 0;
+	}
+
+	int totalFuel = (localAB + localBC) * fuelConsumption;
+
+	cout << "Total fuel consumption: " << totalFuel << " liters" << endl;
 
 	system("pause");
-	return 0
+	return 0;
 }
