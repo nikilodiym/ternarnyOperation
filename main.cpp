9714b8bfb1554268ed8fc205c4a79240bffa6076
Nikita Krenov <nikitabaram@gmail.com>
2024-04-27 21:40:08 +0200

    ex4 number selected

--- main.cpp
+++ main.cpp
@@ -3,8 +3,26 @@
 using namespace std;
 
 int main() {
+    int num1, num2, temp;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    if (num1 == num2) {
+        cout << "The entered numbers are equal." << endl;
+    }
+    else {
+
+        if (num1 > num2) {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        cout << "Numbers in ascending order: " << num1 << ", " << num2 << endl;
+    }
 
 
 	system("pause");
-	return 0
+    return 0;
 }
