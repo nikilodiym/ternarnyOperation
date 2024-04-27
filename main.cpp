3c4566def58cf1e7425685568a7f77da8330009c
Nikita Krenov <nikitabaram@gmail.com>
2024-04-27 16:42:41 +0300

    firts ex in pract forexample number compunity

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main() {
 
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    string result = (number % 2 == 0) ? "steamy" : "odd";
+    cout << "This number is: " << result << endl;
+
 
 	system("pause");
-	return 0
+    return 0;
 }
