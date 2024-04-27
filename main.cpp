c47b7f6bed9ae537546cb6ff2a386bbfa6c16fda
Nikita Krenov <nikitabaram@gmail.com>
2024-04-27 22:17:19 +0200

    where max number

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 
 int main() {
+	int number, maximumNumber, i;
 
+	cout << "Enter number 1: ";
+	cin >> maximumNumber;
+
+	for (i = 2; i <= 7; ++i) {
+		cout << "Enter number " << i << ": ";
+		cin >> number;
+		if (number > maximumNumber) {
+			maximumNumber = number;
+		}
+	}
+
+	cout << "Maximum number is: " << maximumNumber << endl;
 
 	system("pause");
-	return 0
+	return 0;
 }
