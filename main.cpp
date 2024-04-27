3835c49dc5a7d8cb8362c4bef3da5b97b92f4b09
Nikita Krenov <nikitabaram@gmail.com>
2024-04-27 16:50:45 +0300

    ex 2, in this file we can see proj for number what min or max numb

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 
 int main() {
+	int firstNumber, secondNuber;
+	cout << "Enter a first number: ";
+	cin >> firstNumber;
+	
+	cout << "Enter a second number: ";
+	cin >> secondNuber;
 
+	if (firstNumber < secondNuber) {
+		cout << "Smaller number: " << firstNumber << endl;
+	}
+	else if (firstNumber > secondNuber) {
+		cout << "Smaller number: " << secondNuber << endl;
+	}
+	else {
+		cout << "The numbers are the same: " << secondNuber << endl;
+	}
 
 	system("pause");
-	return 0
+	return 0;
 }
